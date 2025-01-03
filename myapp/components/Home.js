ea187cb4df05b14dbec57a16ad8c9ae65a174701
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2025-01-03 22:10:21 +0530

    Add AboutUs page

--- myapp/components/Home.js
+++ myapp/components/Home.js
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, View, ScrollView } from 'react-native';
-
+import { PaperProvider, Text, Divider, Button } from 'react-native-paper';
 
 export default function Home() {
     const navigation = useNavigation();
